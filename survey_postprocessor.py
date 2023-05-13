bf17c984d93eb9b7d604be600528671599c1b5e6
Iannick Gagnon <iannick.gagnon@etsmtl.ca>
2023-05-13 07:29:38 -0400

    Documented postprocessing functions.

--- survey_postprocessor.py
+++ survey_postprocessor.py
@@ -16,55 +16,71 @@ from constants import (
     COURSE_NB_OUTCOMES,
 )
 
-
-def bootstrap_calculate_confidence_interval(base_vector: np.array, alpha: float = 0.05) -> Tuple[float, float]:
+def bootstrap_generate_base_vector_from_bins(bins: Iterable) -> np.array:
     """
-    Calculates (1 - alpha) confidence intervals (CI) for a given dataset.
+    Creates a base vector for bootstrap sampling based on histogram bins. For example, the bins [2, 6, 4] becomes
+    [0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2].
 
     Args:
-      data (Iterable): Data to calculate the CI for.
-      alpha (float): Significance level of the desired interval.
+        bins (Iterable): The histogram bin counts.
 
     Returns:
-      (Tuple[float, float]): Lower and upper bounds of the CI.
+        (np.array): The generated base vector as explained above.
     """
 
-    # Sort in ascending order
-    sorted_data = np.sort(base_vector)
+    # Calculate size of base vector
+    n = sum(bins)
 
-    # (1 - alpha) confidence bounds
-    lower_bound = alpha / 2
-    upper_bound = 1 - lower_bound
+    # Initialize base vector
+    base_vector = np.zeros(n)
 
-    # Confidence bounds indexes
-    lower_index = int(lower_bound * base_vector.shape[0])
-    upper_index = int(upper_bound * base_vector.shape[0])
+    # Build base vector
+    m = 0
+    for i, bin_length in enumerate(bins):
+        for _ in range(bin_length):
+          base_vector[m] = i
+          m += 1
 
-    return sorted_data[lower_index], sorted_data[upper_index]
-
-
-def bootstrap_generate_base_vector_from_bins(bins: Iterable) -> np.array:
-
-  # Calculate size of base vector
-  n = sum(bins)
-
-  # Initialize base vector
-  base_vector = np.zeros(n)
-
-  # Build base vector
-  m = 0
-  for i, bin_length in enumerate(bins):
-    for _ in range(bin_length):
-      base_vector[m] = i
-      m += 1
-
-  return base_vector
+    return base_vector
 
 
 def bootstrap_generate_samples_from_bins(bins: Iterable,
                                          total_count: int,
                                          nb_bins: int,
-                                         nb_samples: int = 1000) -> np.array:
+                                         nb_samples: int = 1000) -> List[np.array]:
+    """
+    Creates bootstrap samples from bins. For example, the bins [2, 6, 4] resul in the base vector
+    [0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2]. This vector is sampled randomly with replacement to generate said bootstrap
+    samples. For example, we might obtain :
+
+        bootstrap sample no.1: [1, 0, 0, 0, 2, 2, 1, 1, 1, 1, 2, 0]
+        bootstrap sample no.2: [1, 0, 1, 2, 2, 2, 1, 1, 0, 1, 2, 1]
+        ...
+        bootstrap sample no.x: [2, 0, 1, 2, 1, 0, 0, 1, 2 ,1, 1, 0]
+
+    In this example, the number of bins (nb_bins) is 3, so we count the number of zeros, the number of ones and the
+    number of twos :
+
+        bootstrap sample no.1: [4, 5, 3]
+        bootstrap sample no.2: [2, 6, 4]
+        ...
+        bootstrap sample no.x: [4, 5, 3]
+
+    Since each column represents a bin and each bin represents a survey element distribution (e.g. a question of an
+    answer), the samples are split column-wise and returned so that confidence intervals can be calculated for each :
+
+        [[4, 2, ..., 4], [5, 6, ..., 5], [3, 4, ..., 3]]
+
+    Args:
+        bins (Iterable): Bin counts.
+        total_count (int): The sum of bin counts.
+        nb_bins (int): The number of bins.
+        nb_samples (int, optional): The number of bootstrap samples to generate. Defaults to 1e3.
+
+    Returns:
+        (List[np.array]): The generated bootstrap samples for each bin.
+    """
+
     # Generate base vector for bootsrap
     base = bootstrap_generate_base_vector_from_bins(bins)
 
@@ -87,28 +103,69 @@ def bootstrap_generate_samples_from_bins(bins: Iterable,
     return columns
 
 
+def bootstrap_calculate_confidence_interval(base_vector: np.array, alpha: float = 0.05) -> Tuple[float, float]:
+    """
+    Calculates (1 - alpha) confidence intervals (CI) for a given dataset.
+
+    Args:
+      base_vector (np.array): Data to calculate the CI for.
+      alpha (float): Significance level of the desired interval.
+
+    Returns:
+      (Tuple[float, float]): Lower and upper bounds of the CI.
+    """
+
+    # Sort in ascending order
+    sorted_data = np.sort(base_vector)
+
+    # (1 - alpha) confidence bounds
+    lower_bound = alpha / 2
+    upper_bound = 1 - lower_bound
+
+    # Confidence bounds indexes
+    lower_index = int(lower_bound * base_vector.shape[0])
+    upper_index = int(upper_bound * base_vector.shape[0])
+
+    return sorted_data[lower_index], sorted_data[upper_index]
+
+
 def confidence_interval_to_string(value: (int, float),
                                   lower_bound: (int, float),
                                   upper_bound: (int, float),
-                                  bar_length: int = 100):
+                                  width: int = 100):
+    """
+    Converts a confidence interval to a string representation.
+
+    Args:
+        value (int or float): The value within the confidence interval.
+        lower_bound (int or float): The lower bound of the confidence interval.
+        upper_bound (int or float): The upper bound of the confidence interval.
+        width (int, optional): The length of the visual bar. Defaults to 100.
+
+    Returns:
+        (str): The string representation of the confidence interval.
+    """
 
     if lower_bound == upper_bound:
-       return ''
+        return ''
 
-    # Determine position of value
-    position_val = int(bar_length * value / (bar_length * 2) )
-    position_lower_bound = int(bar_length * lower_bound / (bar_length * 2))
-    position_upper_bound = int(bar_length * upper_bound / (bar_length * 2))
+    # Determine position of the value and the bounds
+    position_val = int(width * value / (width * 2))
+    position_lower_bound = int(width * lower_bound / (width * 2))
+    position_upper_bound = int(width * upper_bound / (width * 2))
 
+    # Store positions and corresponding symbols
     positions = (position_val, position_lower_bound, position_upper_bound)
     symbols = ('*', '|', '|')
 
-    confidence_interval_string = '-' * bar_length
+    # Initialize string representation
+    confidence_interval_string = '-' * width
 
+    # Add symbols
     for position, symbol in zip(positions, symbols):
         confidence_interval_string = confidence_interval_string[:position] + symbol + confidence_interval_string[position + 1:]
 
-    # Build CI string
+    # Add end caps
     return '[' + confidence_interval_string + ']'
 
 
